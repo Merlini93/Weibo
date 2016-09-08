ddcf7f3fc096753373946fa3bf2d3f06001137bd
李遨东 <Merlini@liaodongdeMacBook-Pro.local>
2016-09-08 13:06:34 +0800

    添加四个导航栏控制器,并提出了一个疑问

--- Weibo/Weibo/Classes/Main/MainViewController.swift
+++ Weibo/Weibo/Classes/Main/MainViewController.swift
@@ -13,23 +13,26 @@ class MainViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tabBar.tintColor = UIColor.orangeColor()
+        
+        addChildViewController(HomeTableViewController(), title: "首页", imageName: "tabbar_home")
+        addChildViewController(MessageTableViewController(), title: "消息", imageName: "tabbar_message_center")
+        addChildViewController(DiscoverTableViewController(), title: "广场", imageName: "tabbar_discover")
+        addChildViewController(ProfileTableViewController(), title: "我", imageName: "tabbar_profile")
+    }
+    
+    private func addChildViewController(childController: UIViewController, title:String, imageName: String) {
+        childController.tabBarItem.image = UIImage(named: imageName)
+        childController.tabBarItem.selectedImage = UIImage(named: "\(imageName)_highlighted")
+        childController.title = title
+        let navi = UINavigationController()
+        navi.addChildViewController(childController)
+        addChildViewController(navi)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
